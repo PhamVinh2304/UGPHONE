12d13b2a7e768cb9a8a99635b2de9a761a27e8ea
PhamVinh2304 <frajikebmgo@gmail.com>
2024-12-01 15:13:10 +0700

    Update trackstats.lua

--- trackstats.lua
+++ trackstats.lua
@@ -1,17 +1,18 @@
+
 repeat task.wait() until game:IsLoaded()
 getgenv().Hermanos_Settings = {
 	['key'] = '150013ed-df37-4424-abd3-b66a936c4d84',
 	['PC'] = 'DDC-1',
-Tushita
+
 	['webhooks'] = {
 		['fullmoon'] = 'https://discord.com/api/webhooks/',
 		['mirage'] = 'https://discord.com/api/webhooks/',
 	},
 
-	['Sword'] = {'Cursed Dual Katana', 'Shark Anchor', '', 'Yama', 'Dark Dagger', 'Hallow Scythe', 'Saber'},
+	['Sword'] = {'Cursed Dual Katana', 'Shark Anchor', 'Tushita', 'Yama', 'Dark Dagger', 'Hallow Scythe', 'Saber'},
 	['Gun'] = {'Soul Guitar', 'Serpent Bow', 'Kabucha', 'Acidum Rifle'},
 	['Accessories'] = {'Dark Coat', 'Leviathan Shield','Leviathan Crown', 'Pale Scarf', 'Kitsune Mask', 'Kitsune Ribbon'},
 	['Fruit'] = {'Kitsune', 'Leopard', 'Dragon',  'Spirit', 'Control', 'Venom', 'Shadow', 'Dough','Mammoth', 'T-Rex'},
 }
 task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/script.lua'))() end)
-if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+	
