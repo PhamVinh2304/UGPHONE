c838dab11c31e78914afa3c1c7f43be5fedd62c7
PhamVinh2304 <frajikebmgo@gmail.com>
2024-12-24 20:41:57 +0700

    Update trackstats.lua

--- trackstats.lua
+++ trackstats.lua
@@ -1,18 +1,2 @@
-
-repeat task.wait() until game:IsLoaded()
-getgenv().Hermanos_Settings = {
-	['key'] = '150013ed-df37-4424-abd3-b66a936c4d84',
-	['PC'] = 'DDC-1',
-
-	['webhooks'] = {
-		['fullmoon'] = 'https://discord.com/api/webhooks/',
-		['mirage'] = 'https://discord.com/api/webhooks/',
-	},
-
-	['Sword'] = {'Cursed Dual Katana', 'Shark Anchor', 'Tushita', 'Yama', 'Dark Dagger', 'Hallow Scythe', 'Saber'},
-	['Gun'] = {'Soul Guitar', 'Serpent Bow', 'Kabucha', 'Acidum Rifle'},
-	['Accessories'] = {'Dark Coat', 'Leviathan Shield','Leviathan Crown', 'Pale Scarf', 'Kitsune Mask', 'Kitsune Ribbon'},
-	['Fruit'] = {'Kitsune', 'Leopard', 'Dragon',  'Spirit', 'Control', 'Venom', 'Shadow', 'Dough','Mammoth', 'T-Rex'},
-}
-task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/script.lua'))() end)
-	
+getgenv().ScriptKey = "fe98d20dae227c845e"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nilprovai/Rokid-Trackstats/refs/heads/main/script.lua"))()
